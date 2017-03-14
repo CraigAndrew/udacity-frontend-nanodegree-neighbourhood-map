cf7413f738bc1d21a8433a2f0af6a829445ff3ae
Craig Andrew <craig.andrew@chep.com>
2017-03-14 22:58:30 +0200

    Added foundation files.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Neighbourhood Map project for Udacity Front-End Nanodegree
+
+Starter code for neighbourhood map project.
+
+
+# Installing
+
+1. `npm install -g gulp` to install Gulp globally.
+2. `npm install --save google-maps-api-loader`
+3. `npm install` to resolve project dependencies.
+
+# Using
+
+Run `gulp` from the command line and you are good to go!
+
+The project is currently setup to transpile code under the _/src_ folder using the
+_/src/app.js_ file as an entry point.
+
---
+++ src/app.js
@@ -0,0 +1,58 @@
+// Entry Point for application.
+const defaultLat = -29.83608;
+const defaultLng = 30.918399;
+const googleApiKey = 'AIzaSyC_77S5Ozh5RMPEQ98QBA9iOSHPQxZM_N8';
+const fourSquareClientId = '0FD1PHV1YKMHSMF0T1M1PFIFLWRB12EQAGRDIK5Z2WOJOVNQ';
+const fourSquareClientSecret = 'XXASVO0SW14RJKNE0ETMNNATAPQVBO0PPJA5WFNATBPW3J3L';
+
+/**
+ * Temporary init map function.
+ */
+function initMap() {
+  var latLng = {lat: defaultLat, lng: defaultLng};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: latLng,
+    zoom: 10
+  });
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+}
+
+function loadPlacesDataList() {
+  const placesArr = ['abc', 'def'];
+
+  // Loop over the JSON array.
+  placesArr.forEach(function (place) {
+    // Get the <datalist> and <input> elements.
+    var dataList = document.getElementById('data-places');
+    var input = document.getElementById('places');
+
+    // Create a new <option> element.
+    var option = document.createElement('option');
+    // Set the value using the item in the JSON array.
+    option.value = place;
+    // Add the <option> element to the <datalist>.
+    dataList.append(option);
+  });
+}
+
+var GoogleMapsApiLoader = require('google-maps-api-loader');
+
+GoogleMapsApiLoader({
+  libraries: ['places'],
+  apiKey: 'AIzaSyC_77S5Ozh5RMPEQ98QBA9iOSHPQxZM_N8'
+}).then(function (googleApi) {
+  console.log('andrewc');
+  initMap();
+  var autocomplete = new googleApi.maps.places.AutocompleteService();
+
+  loadPlacesDataList();
+  console.log('andrewc dataList', dataList);
+
+  // Update the placeholder text.
+  input.placeholder = "e.g. datalist";
+}, function (err) {
+  console.error(err);
+});
