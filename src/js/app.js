00cf71f14310647b77f5bfb48449a2f61019828d
Craig Andrew <craig.andrew@chep.com>
2017-04-04 15:28:51 +0200

    Convert external to internal resources.

--- src/js/app.js
+++ src/js/app.js
@@ -197,13 +197,17 @@ const googleApiKey = 'AIzaSyC_77S5Ozh5RMPEQ98QBA9iOSHPQxZM_N8';
 //   console.error(err);
 // });
 console.log('1');
-let locationsModel;
+let placesModel;
 
-function toggleBounce(marker) {
-  if (marker.getAnimation() !== null) {
+function toggleMarkerBounceAnimation(marker) {
+  toggleMarkerAnimation(marker, google.maps.Animation.BOUNCE);
+}
+
+function toggleMarkerAnimation(marker, animation) {
+  if (!_.isNull(marker.getAnimation())) {
     marker.setAnimation(null);
   } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
+    marker.setAnimation(animation);
   }
 }
 
@@ -215,7 +219,7 @@ GoogleMapsApiLoader({
   initialize();
   console.log('3');
 
-  setupLocations();
+  initializePlaces();
   // var autocomplete = new googleApi.maps.places.AutocompleteService();
   //
   // loadPlacesDataList();
@@ -258,8 +262,8 @@ function initialize() {
   mapOptions);
 }
 
-// Location Class completely builds everything needed for each location marker.
-var Location = function(title, lng, lat, venueId, cat) {
+// Place Class completely builds everything needed for each location marker.
+var Place = function(title, lng, lat, venueId, cat) {
   var self = this;
   this.title = title;
   this.lng = lng;
@@ -297,15 +301,16 @@ var Location = function(title, lng, lat, venueId, cat) {
 
   // Assigns a marker icon color based on the category of the location.
   switch (this.cat) {
-    case "Shopping":
-      this.icon = 'http://www.googlemapsmarkers.com/v1/009900/';
+    case "shop":
+      this.icon = 'src/css/img/shop.png';
       break;
-    case "Food":
-      this.icon = 'http://www.googlemapsmarkers.com/v1/0099FF/';
+    case "eat":
+      this.icon = 'src/css/img/eat.png';
       break;
     default:
-      this.icon = 'http://www.googlemapsmarkers.com/v1/990000/';
+      this.icon = 'src/css/img/default.png';
   }
+
   this.marker = new google.maps.Marker({
     position: new google.maps.LatLng(self.lng, self.lat),
     map: map,
@@ -315,56 +320,52 @@ var Location = function(title, lng, lat, venueId, cat) {
 
   // Opens the info window for the location marker.
   this.openInfowindow = function() {
-    for (var i=0; i < locationsModel.locations.length; i++) {
-      locationsModel.locations[i].infowindow.close();
+    for (var i=0; i < placesModel.locations.length; i++) {
+      placesModel.locations[i].infowindow.close();
     }
     map.panTo(self.marker.getPosition())
     self.infowindow.setContent(self.content);
     self.infowindow.open(map,self.marker);
-    toggleBounce(this.marker);
+    toggleMarkerBounceAnimation(this.marker);
   };
 
   // Assigns a click event listener to the marker to open the info window.
   this.addListener = google.maps.event.addListener(self.marker,'click', (this.openInfowindow));
-  console.log('Location end');
+  console.log('Place end');
 };
 
 // Contains all the locations and search function.
-locationsModel = {
+placesModel = {
   locations:[],
   query: ko.observable(''),
 };
 
 // Search function for filtering through the list of locations based on the name of the location.
-locationsModel.search = ko.dependentObservable(function() {
-  console.log('search', locationsModel.locations);
+placesModel.search = ko.dependentObservable(function() {
+  console.log('search', placesModel.locations);
 
   var self = this;
   var search = this.query().toLowerCase();
   return ko.utils.arrayFilter(self.locations, function(location) {
     return location.title.toLowerCase().indexOf(search) >= 0;
   });
-}, locationsModel);
+}, placesModel);
 
-function setupLocations() {
-  console.log('setupLocations');
-  locationsModel.locations = [
-  new Location('Penzeys Spices', 35.78961, -78.66032, '4cdd6918d4ecb1f701298548', 'Shopping'),
-      new Location('Raleigh Flea Market', 35.79499, -78.70719, '4ad4c00af964a5203ded20e3', 'Shopping'),
-      new Location('Reader\'s Corner', 35.79005, -78.67937, '4adc8051f964a520b92c21e3', 'Shopping'),
-      new Location('State Farmers Market', 35.76363, -78.66274, '4bb8979c3db7b713c965219a', 'Shopping'),
-      new Location('Capital RunWalk', 35.79039, -78.65867, '4b6b5120f964a52078002ce3', 'Shopping'),
-      new Location('Watson\'s Market Place & Flea Market', 35.76063, -78.61596, '4d615493e4fe5481a8618a9e', 'Shopping'),
-      new Location('Raleigh Denim', 35.77665, -78.64465, '4c84e24574d7b60ca66196d8', 'Shopping'),
-      new Location('Cloos\' Coney Island', 35.77770, -78.67485, '4afee1fdf964a520333122e3', 'Food'),
-      new Location('Franks Pizza', 35.77880, -78.60704, '4aecb1b2f964a52056ca21e3', 'Food'),
-      new Location('Snoopy\'s Hot Dogs & More', 35.80719, -78.62499, '4bf6c03013aed13a6823eaf7', 'Food'),
-      new Location('Clyde Cooper\'s Barbecue', 35.77630, -78.63831, '4b63170ef964a52039622ae3', 'Food')
+function initializePlaces() {
+  console.log('initializePlaces');
+  placesModel.locations = [
+    new Place('The Pavilion Shopping Center', -29.849002300639423, 30.93577734073859, '4cdd6918d4ecb1f701298548', 'Shopping'),
+    new Place('Westville Mall', -29.83608, 30.918399, '4ad4c00af964a5203ded20e3', 'Shopping'),
+    new Place('Kauai', -29.83608, 30.918399, '4adc8051f964a520b92c21e3', 'Shopping'),
+    new Place('Olive & Oil Cafe', 29.839529871456172, 30.925247375447384, '4bb8979c3db7b713c965219a', 'Shopping'),
+    new Place('Waxy O\'Connors', -29.827756663602152, 30.929725103495258, '4b6b5120f964a52078002ce3', 'Shopping'),
+    new Place('Lupa Osteria', -29.8277474062012, 30.930414401226106, '4d615493e4fe5481a8618a9e', 'Shopping'),
+    new Place('Chez nous', -29.836469892379846, 30.91703684659349, '4c84e24574d7b60ca66196d8', 'Shopping')
   ];
-  console.log('setupLocations End', locationsModel.locations);
+  console.log('initializePlaces End', placesModel.locations);
 }
 
-ko.applyBindings(locationsModel);
+ko.applyBindings(placesModel);
 
 // TODO: Filter markers on input type
 // TODO: When clicking on marker highlight item in listview
