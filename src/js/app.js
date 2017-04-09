50ad755ffe9b2e47fc711c86302581ecb0f5490b
Craig Andrew <craig.andrew@chep.com>
2017-04-09 15:33:56 +0200

    Removed commented out code.

--- src/js/app.js
+++ src/js/app.js
@@ -36,9 +36,9 @@ GoogleMapsApiLoader({
 }).then(function (googleApi) {
   initialize();
   initializePlaces();
-  const autocomplete = new googleApi.maps.places.AutocompleteService();
-  // Update the placeholder text.
-  // input.placeholder = "e.g. datalist";
+  $("#search-here").click(() => {
+    $("#search-area").toggle();
+  });
 }, function (err) {
   console.error(err);
 });
@@ -102,7 +102,6 @@ let Place = function(name, cat, lng, lat) {
 
   // Opens the info window for the location marker.
   this.openInfowindow = function() {
-    console.log('openInfowindow');
     map.setCenter(_this.marker.getPosition());
     for (let i=0; i < placesModel.locations.length; i++) {
       placesModel.locations[i].infowindow.close();
@@ -170,183 +169,7 @@ function initializePlaces() {
 ko.applyBindings(placesModel);
 
 // TODO: Responsive design, mobile-first if you can
+// TODO: Bring up error message if scripts fail to load
 // TODO: Google Autocomplete
 // NICE TO HAVE: MINIFY JS, CSS etc
 // TODO: When clicking on marker highlight item in listview
-
-
-// const observablePlaces = ko.observableArray(places);
-// const defaultZoomLevel = 15;
-// let map;
-//
-// /**
-//  * Temporary init map function.
-//  */
-// function initMap() {
-//   let latLng = {lat: defaultLat, lng: defaultLng};
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: latLng,
-//     zoom: defaultZoomLevel
-//   });
-//
-//   places.forEach(({ name, lat, lng })=> {
-//     const marker = new google.maps.Marker({
-//       position: { lat, lng },
-//       map: map
-//     });
-//     marker.addListener('click', () => {
-//       const place = _.find(places, { name });
-//       if (place) {
-//         moveToMarker(place);
-//       }
-//     });
-//   });
-// }
-//
-// function loadPlacesDataList() {
-//   // Get the <datalist> and <input> elements.
-//   let dataList = document.getElementById('dataPlaces');
-//   let input = document.getElementById('places');
-//
-//   // Loop over the JSON array.
-//   places.forEach(function ({ name }) {
-//     // Create a new <option> element.
-//     let option = document.createElement('option');
-//     // Set the value using the item in the JSON array.
-//     option.value = name;
-//
-//     // Add the <option> element to the <datalist>.
-//     dataList.append(option);
-//   });
-// }
-//
-// function moveToMarker(place) {
-//   map.panTo( new google.maps.LatLng( place.lat, place.lng ) );
-//   const marker = new google.maps.Marker({
-//     position: {
-//       lat: place.lat,
-//       lng: place.lng
-//     },
-//     map
-//   });
-//
-//   const fourSquareDetails = getFourSquareDetails(marker, place);
-// }
-//
-// /**
-//  * Gets details via Four Square API
-//  */
-// function getFourSquareDetails(marker, place) {
-//   let url = `https://api.foursquare.com/v2/venues/search?client_id=${fourSquareClientId}&client_secret=${fourSquareClientSecret}&v=20130815&ll=
-//     ${place.lat},${place.lng}&query=\'${place.name}\'&limit=1`;
-//
-//   const getFourSquareDetailsForPlacePromise = new Promise((resolve, reject) => {
-//     $.getJSON(url).done(function(response) {
-//       console.log('response', response);
-//       resolve(formatFourSquareDetailsForPlaceMarkerDisplay(response));
-//     });
-//   });
-//
-//   function formatFourSquareDetailsForPlaceMarkerDisplay(response) {
-//     let fourSquareStr = '<p>Foursquare info:<br>';
-//     let venue = response.response.venues[0];
-//     let venueId = venue.id;
-//     let venueName = venue.name;
-//
-//     if (venueName !== null && venueName !== undefined) {
-//       fourSquareStr = fourSquareStr + 'name: ' + venueName + '<br>';
-//     }
-//
-//     let phoneNum = venue.contact.formattedPhone;
-//     if (phoneNum !== null && phoneNum !== undefined) {
-//       fourSquareStr = fourSquareStr + 'phone: ' + phoneNum + '<br>';
-//     }
-//
-//     let twitterId = venue.contact.twitter;
-//     if (twitterId !== null && twitterId !== undefined) {
-//       fourSquareStr = fourSquareStr + 'twitter name: ' +
-//           twitterId + '<br>';
-//     }
-//
-//     let address = venue.location.formattedAddress;
-//     if (address !== null && address !== undefined) {
-//       fourSquareStr = fourSquareStr + 'address: ' + address + '<br>';
-//     }
-//
-//     let category = venue.categories.shortName;
-//     if (category !== null && category !== undefined) {
-//       fourSquareStr = fourSquareStr + 'category: ' + category + '<br>';
-//     }
-//
-//     let checkinCount = venue.stats.checkinsCount;
-//     if (checkinCount !== null && checkinCount !== undefined) {
-//       fourSquareStr = fourSquareStr + '# of checkins: ' + checkinCount + '<br>';
-//     }
-//
-//     let tipCount = venue.stats.tipCount;
-//
-//     // if (tipCount > 0) {
-//     //   _this.getFourSquareTips(venueId, pt);
-//     // } else {
-//     //   fourSquareStr = fourSquareStr + '</p>';
-//     //   _this.checkPano();
-//     // }
-//
-//     console.log('andrewcfoursquare2', fourSquareStr);
-//     return fourSquareStr;
-//   }
-//
-//   getFourSquareDetailsForPlacePromise
-//     .then((response) => {
-//       const infoWindow = new google.maps.InfoWindow({
-//         content: response
-//       });
-//       infoWindow.open(map, marker);
-//     });
-//
-//   // $.getJSON(url).done(function(response) {
-//   //   console.log('response', response);
-//   //     .fail(function() {
-//   //       fourSquareStr = 'Four square data request failed';
-//   //       console.error('Four square failed to load information' +
-//   //           'attempting to load error  we can get into info window');
-//   //       _this.checkPano();
-//   //     });
-// };
-//
-// // Here's my data model
-// let ViewModel = function(first, last) {
-//   this.dataPlaces = ko.observableArray([]);
-//   this.firstName = ko.observable(first);
-//   this.lastName = ko.observable(last);
-//   this.text = ko.observable();
-//   this.filter =  ko.observable("");
-//   this.updated = ko.computed(function (val) {
-//     console.log('val', val);
-//   });
-//   this.fullName = ko.pureComputed(function() {
-//     // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-//     return this.firstName() + " " + this.lastName();
-//   }, this);
-//
-//   this.test = function() {
-//     console.log('andrewc test');
-//   };
-// };
-//
-// ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
-//
-// GoogleMapsApiLoader({
-//   libraries: ['places'],
-//   apiKey: googleApiKey
-// }).then(function (googleApi) {
-//   initMap();
-//   let autocomplete = new googleApi.maps.places.AutocompleteService();
-//
-//   loadPlacesDataList();
-//
-//   // Update the placeholder text.
-//   input.placeholder = "e.g. datalist";
-// }, function (err) {
-//   console.error(err);
-// });
