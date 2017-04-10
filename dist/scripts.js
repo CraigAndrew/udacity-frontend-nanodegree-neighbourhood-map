d13a3dd154c3cacb6b28f1b09689c81185301437
Craig Andrew <craig.andrew@chep.com>
2017-04-10 13:09:54 +0200

    Initial experimental.

--- dist/scripts.js
+++ dist/scripts.js
@@ -29593,6 +29593,7 @@ var fourSquareClientSecret = 'XXASVO0SW14RJKNE0ETMNNATAPQVBO0PPJA5WFNATBPW3J3L';
 var googleApiKey = 'AIzaSyC_77S5Ozh5RMPEQ98QBA9iOSHPQxZM_N8';
 var map = void 0;
 var placesModel = void 0;
+var infowindow = void 0;
 
 /**
  *
@@ -29617,42 +29618,6 @@ function toggleMarkerAnimation(marker, animation) {
 
 /**
  *
- */
-(0, _googleMapsApiLoader2.default)({
-  libraries: ['places'],
-  apiKey: googleApiKey
-}).then(function (googleApi) {
-  initialize();
-  initializePlaces();
-  (0, _jquery2.default)("div.search-area").click(function () {
-    console.log('click');
-    console.log((0, _jquery2.default)('span#arrow').html());
-    (0, _jquery2.default)("ul").slideToggle();
-
-    _lodash2.default.defer(function () {
-      if ((0, _jquery2.default)('span#arrow').html() === '▼') {
-        (0, _jquery2.default)('span#arrow').html('▲');
-        (0, _jquery2.default)('div.search-area').css({ 'width': '100%' });
-      } else {
-        (0, _jquery2.default)('span#arrow').html('▼');
-        (0, _jquery2.default)('div.search-area').css({ 'width': 'auto' });
-      }
-    });
-  });
-  (0, _jquery2.default)(window).resize(function () {
-    console.log('window resized');
-    if (isMobile()) {
-      (0, _jquery2.default)("ul").slideUp();
-    } else {
-      (0, _jquery2.default)("ul").slideDown();
-    }
-  });
-}, function (err) {
-  console.error(err);
-});
-
-/**
- *
  * @returns {boolean}
  */
 function isMobile() {
@@ -29663,7 +29628,7 @@ function isMobile() {
 /**
  *
  */
-function initialize() {
+function initializeMap() {
   var mapOptions = {
     zoom: defaultZoomLevel,
     center: new google.maps.LatLng(defaultLat, defaultLng),
@@ -29708,77 +29673,75 @@ var Place = function Place(name, cat, lng, lat) {
       console.log('getJSON request failed! ' + textStatus);
     });
   }();
-
-  this.infowindow = new google.maps.InfoWindow();
-
-  // Assigns a marker icon color based on the category of the location.
-  var imgPath = 'src/css/img/';
-  switch (this.cat) {
-    case "eat":
-      this.icon = imgPath + 'eat.png';
-      break;
-    case "shop":
-      this.icon = imgPath + 'shop.png';
-      break;
-    default:
-      this.icon = imgPath + 'default.png';
-  }
-
-  this.marker = new google.maps.Marker({
-    position: new google.maps.LatLng(_this.lng, _this.lat),
-    animation: google.maps.Animation.DROP,
-    map: map,
-    name: _this.name,
-    icon: _this.icon
-  });
-
-  // Opens the info window for the location marker.
-  /**
-   *
-   */
-  this.openInfowindow = function () {
-    map.setCenter(_this.marker.getPosition());
-    for (var i = 0; i < placesModel.locations.length; i++) {
-      placesModel.locations[i].infowindow.close();
-    }
-    map.panTo(_this.marker.getPosition());
-    _this.infowindow.setContent(_this.content);
-    _this.infowindow.open(map, _this.marker);
-    _lodash2.default.defer(function () {
-      return toggleMarkerBounceAnimation(_this.marker);
-    });
-  };
-
-  /**
-   *
-   */
-  this.highlightPlace = function () {
-    _this.marker.setIcon(imgPath + 'active.png');
-  };
-
-  /**
-   *
-   */
-  this.unhighlightPlace = function () {
-    switch (_this.cat) {
-      case "eat":
-        _this.marker.setIcon(imgPath + 'eat.png');
-        break;
-      case "shop":
-        _this.marker.setIcon(imgPath + 'shop.png');
-        break;
-      default:
-        _this.marker.setIcon(imgPath + 'default.png');
-    }
-  };
-
-  // Assigns a click event listener to the marker to open the info window.
-  this.addListener = google.maps.event.addListener(_this.marker, 'click', this.openInfowindow);
+  //
+  //   this.infowindow = new google.maps.InfoWindow();
+  //
+  //   // Assigns a marker icon color based on the category of the location.
+  //   const imgPath = 'src/css/img/';
+  //   switch (this.cat) {
+  //     case "eat":
+  //       this.icon = imgPath + 'eat.png';
+  //       break;
+  //     case "shop":
+  //       this.icon = imgPath + 'shop.png';
+  //       break;
+  //     default:
+  //       this.icon = imgPath + 'default.png';
+  //   }
+  //
+  //   this.marker = new google.maps.Marker({
+  //     position: new google.maps.LatLng(_this.lng, _this.lat),
+  //     animation: google.maps.Animation.DROP,
+  //     map,
+  //     name: _this.name,
+  //     icon: _this.icon
+  //   });
+  //
+  //   // Opens the info window for the location marker.
+  //   /**
+  //    *
+  //    */
+  //   this.openInfowindow = function() {
+  //     map.setCenter(_this.marker.getPosition());
+  //     for (let i=0; i < placesModel.locations.length; i++) {
+  //       placesModel.locations[i].infowindow.close();
+  //     }
+  //     map.panTo(_this.marker.getPosition())
+  //     _this.infowindow.setContent(_this.content);
+  //     _this.infowindow.open(map,_this.marker);
+  //     _.defer(() => toggleMarkerBounceAnimation(_this.marker));
+  //   };
+  //
+  //   /**
+  //    *
+  //    */
+  //   this.highlightPlace = function() {
+  //     _this.marker.setIcon(imgPath + 'active.png');
+  //   }
+  //
+  //   /**
+  //    *
+  //    */
+  //   this.unhighlightPlace = function() {
+  //     switch (_this.cat) {
+  //       case "eat":
+  //         _this.marker.setIcon(imgPath + 'eat.png');
+  //         break;
+  //       case "shop":
+  //         _this.marker.setIcon(imgPath + 'shop.png');
+  //         break;
+  //       default:
+  //         _this.marker.setIcon(imgPath + 'default.png');
+  //     }
+  //   }
+  //
+  //   // Assigns a click event listener to the marker to open the info window.
+  //   this.addListener = google.maps.event.addListener(_this.marker,'click', (this.openInfowindow));
 };
 
 // Contains all the locations and search function.
 placesModel = {
-  locations: [],
+  locations: [new Place('The Pavilion Shopping Center', 'shop', -29.849002300639423, 30.93577734073859), new Place('Westville Mall', 'shop', -29.83608, 30.918399), new Place('Kauai', 'eat', -29.83608, 30.918399), new Place('Olive & Oil Cafe', 'eat', 29.839529871456172, 30.925247375447384), new Place('Waxy O\'Connors', 'eat', -29.827756663602152, 30.929725103495258), new Place('Lupa Osteria', 'eat', -29.8277474062012, 30.930414401226106), new Place('Chez nous', 'eat', -29.836469892379846, 30.91703684659349)],
   query: _knockout2.default.observable('')
 };
 
@@ -29786,29 +29749,116 @@ placesModel = {
  *
  */
 // Search function for filtering through the list of locations based on the name of the location.
-placesModel.search = _knockout2.default.dependentObservable(function () {
+placesModel.search = _knockout2.default.computed(function () {
   var _this = this;
   var search = this.query().toLowerCase();
-  return _knockout2.default.utils.arrayFilter(_this.locations, function (location) {
+  var searchResults = _knockout2.default.utils.arrayFilter(_this.locations, function (location) {
     var match = location.name.toLowerCase().indexOf(search) >= 0;
     if (!match) {
-      location.marker.setVisible(false);
+      // location.marker.setVisible(false);
     } else {
-      location.marker.setVisible(true);
-    }
+        // location.marker.setVisible(true);
+      }
     return match;
   });
+
+  console.log('searchResults', searchResults);
+  if (_lodash2.default.isEmpty) {
+    searchResults = placesModel.locations;
+    console.log('searchResults default', searchResults);
+  }
+
+  return searchResults;
 }, placesModel);
 
 /**
  *
  */
 function initializePlaces() {
-  placesModel.locations = [new Place('The Pavilion Shopping Center', 'shop', -29.849002300639423, 30.93577734073859), new Place('Westville Mall', 'shop', -29.83608, 30.918399), new Place('Kauai', 'eat', -29.83608, 30.918399), new Place('Olive & Oil Cafe', 'eat', 29.839529871456172, 30.925247375447384), new Place('Waxy O\'Connors', 'eat', -29.827756663602152, 30.929725103495258), new Place('Lupa Osteria', 'eat', -29.8277474062012, 30.930414401226106), new Place('Chez nous', 'eat', -29.836469892379846, 30.91703684659349)];
+  // placesModel.locations = [
+  //   new Place('The Pavilion Shopping Center', 'shop', -29.849002300639423, 30.93577734073859),
+  //   new Place('Westville Mall', 'shop', -29.83608, 30.918399),
+  //   new Place('Kauai', 'eat', -29.83608, 30.918399),
+  //   new Place('Olive & Oil Cafe', 'eat', 29.839529871456172, 30.925247375447384),
+  //   new Place('Waxy O\'Connors', 'eat', -29.827756663602152, 30.929725103495258),
+  //   new Place('Lupa Osteria', 'eat', -29.8277474062012, 30.930414401226106),
+  //   new Place('Chez nous', 'eat', -29.836469892379846, 30.91703684659349)
+  // ];
 }
+
+function setupMarkersForPlaces() {
+  _lodash2.default.forEach(placesModel.locations, function (location) {
+    location.marker = new google.maps.Marker({
+      position: new google.maps.LatLng(location.lng, location.lat),
+      animation: google.maps.Animation.DROP,
+      map: map,
+      name: location.name,
+      icon: location.icon
+    });
+
+    console.log('marker1 in loop', location.marker);
+    location.marker.infowindow = new google.maps.InfoWindow();
+
+    // Assigns a click event listener to the marker to open the info window.
+    location.marker.addListener = google.maps.event.addListener(location.marker, 'click', function () {
+      map.setCenter(location.marker.getPosition());
+      for (var i = 0; i < placesModel.locations.length; i++) {
+        if (placesModel.locations[i].marker.infowindow) {
+          placesModel.locations[i].marker.infowindow.close();
+        }
+      }
+      map.panTo(location.marker.getPosition());
+      location.marker.infowindow.setContent(location.content);
+      location.marker.infowindow.open(map, location.marker);
+      _lodash2.default.defer(function () {
+        return toggleMarkerBounceAnimation(location.marker);
+      });
+    });
+  });
+}
+
+/**
+ *
+ */
+(0, _googleMapsApiLoader2.default)({
+  libraries: ['places'],
+  apiKey: googleApiKey
+}).then(function (googleApi) {
+  initializeMap();
+  initializePlaces();
+  setupMarkersForPlaces();
+
+  (0, _jquery2.default)("span#arrow").click(function () {
+    console.log('click');
+    console.log((0, _jquery2.default)('span#arrow').html());
+    (0, _jquery2.default)("ul").slideToggle();
+
+    if ((0, _jquery2.default)('span#arrow').html() === '▼') {
+      (0, _jquery2.default)('span#arrow').html('▲');
+      (0, _jquery2.default)('div.search-area').css({ 'width': '100%' });
+    } else {
+      (0, _jquery2.default)('span#arrow').html('▼');
+      (0, _jquery2.default)('div.search-area').css({ 'width': 'auto' });
+    }
+  });
+  (0, _jquery2.default)(window).resize(function () {
+    console.log('window resized');
+    if (isMobile()) {
+      (0, _jquery2.default)("ul").slideUp();
+    } else {
+      (0, _jquery2.default)("ul").slideDown();
+    }
+  });
+
+  console.log('okay slidedown');
+  (0, _jquery2.default)("ul").slideDown();
+}, function (err) {
+  console.error(err);
+});
 
 _knockout2.default.applyBindings(placesModel);
 
+// TODO: Decouple 3rd party APIs and model
 // TODO: Responsive design, mobile-first if you can
 // NICE TO HAVE: MINIFY JS, CSS etc
 // TODO: When clicking on marker highlight item in listview
--- src/js/app.js
+++ src/js/app.js
@@ -17,6 +17,7 @@ const fourSquareClientSecret = 'XXASVO0SW14RJKNE0ETMNNATAPQVBO0PPJA5WFNATBPW3J3L
 const googleApiKey = 'AIzaSyC_77S5Ozh5RMPEQ98QBA9iOSHPQxZM_N8';
 let map;
 let placesModel;
+let infowindow;
 
 /**
  *
@@ -41,42 +42,6 @@ function toggleMarkerAnimation(marker, animation) {
 
 /**
  *
- */
-GoogleMapsApiLoader({
-  libraries: ['places'],
-  apiKey: googleApiKey
-}).then(function (googleApi) {
-  initialize();
-  initializePlaces();
-  $("div.search-area").click(() => {
-    console.log('click');
-    console.log($('span#arrow').html());
-    $("ul").slideToggle();
-
-    _.defer(() => {
-      if ($('span#arrow').html() === '▼') {
-        $('span#arrow').html('▲');
-        $('div.search-area').css({'width': '100%'});
-      } else {
-        $('span#arrow').html('▼');
-        $('div.search-area').css({'width': 'auto'});
-      }
-    });
-  });
-  $( window ).resize(function() {
-    console.log('window resized');
-    if (isMobile()) {
-      $("ul").slideUp();
-    } else {
-      $("ul").slideDown();
-    }
-  });
-}, function (err) {
-  console.error(err);
-});
-
-/**
- *
  * @returns {boolean}
  */
 function isMobile() {
@@ -86,7 +51,7 @@ function isMobile() {
 /**
  *
  */
-function initialize() {
+function initializeMap() {
   let mapOptions = {
     zoom: defaultZoomLevel,
     center: new google.maps.LatLng(defaultLat, defaultLng),
@@ -130,101 +95,75 @@ let Place = function(name, cat, lng, lat) {
       console.log('getJSON request failed! ' + textStatus);
     });
   }();
-
-  this.infowindow = new google.maps.InfoWindow();
-
-  // Assigns a marker icon color based on the category of the location.
-  const imgPath = 'src/css/img/';
-  switch (this.cat) {
-    case "eat":
-      this.icon = imgPath + 'eat.png';
-      break;
-    case "shop":
-      this.icon = imgPath + 'shop.png';
-      break;
-    default:
-      this.icon = imgPath + 'default.png';
-  }
-
-  this.marker = new google.maps.Marker({
-    position: new google.maps.LatLng(_this.lng, _this.lat),
-    animation: google.maps.Animation.DROP,
-    map,
-    name: _this.name,
-    icon: _this.icon
-  });
-
-  // Opens the info window for the location marker.
-  /**
-   *
-   */
-  this.openInfowindow = function() {
-    map.setCenter(_this.marker.getPosition());
-    for (let i=0; i < placesModel.locations.length; i++) {
-      placesModel.locations[i].infowindow.close();
-    }
-    map.panTo(_this.marker.getPosition())
-    _this.infowindow.setContent(_this.content);
-    _this.infowindow.open(map,_this.marker);
-    _.defer(() => toggleMarkerBounceAnimation(_this.marker));
-  };
-
-  /**
-   *
-   */
-  this.highlightPlace = function() {
-    _this.marker.setIcon(imgPath + 'active.png');
-  }
-
-  /**
-   *
-   */
-  this.unhighlightPlace = function() {
-    switch (_this.cat) {
-      case "eat":
-        _this.marker.setIcon(imgPath + 'eat.png');
-        break;
-      case "shop":
-        _this.marker.setIcon(imgPath + 'shop.png');
-        break;
-      default:
-        _this.marker.setIcon(imgPath + 'default.png');
-    }
-  }
-
-  // Assigns a click event listener to the marker to open the info window.
-  this.addListener = google.maps.event.addListener(_this.marker,'click', (this.openInfowindow));
+//
+//   this.infowindow = new google.maps.InfoWindow();
+//
+//   // Assigns a marker icon color based on the category of the location.
+//   const imgPath = 'src/css/img/';
+//   switch (this.cat) {
+//     case "eat":
+//       this.icon = imgPath + 'eat.png';
+//       break;
+//     case "shop":
+//       this.icon = imgPath + 'shop.png';
+//       break;
+//     default:
+//       this.icon = imgPath + 'default.png';
+//   }
+//
+//   this.marker = new google.maps.Marker({
+//     position: new google.maps.LatLng(_this.lng, _this.lat),
+//     animation: google.maps.Animation.DROP,
+//     map,
+//     name: _this.name,
+//     icon: _this.icon
+//   });
+//
+//   // Opens the info window for the location marker.
+//   /**
+//    *
+//    */
+//   this.openInfowindow = function() {
+//     map.setCenter(_this.marker.getPosition());
+//     for (let i=0; i < placesModel.locations.length; i++) {
+//       placesModel.locations[i].infowindow.close();
+//     }
+//     map.panTo(_this.marker.getPosition())
+//     _this.infowindow.setContent(_this.content);
+//     _this.infowindow.open(map,_this.marker);
+//     _.defer(() => toggleMarkerBounceAnimation(_this.marker));
+//   };
+//
+//   /**
+//    *
+//    */
+//   this.highlightPlace = function() {
+//     _this.marker.setIcon(imgPath + 'active.png');
+//   }
+//
+//   /**
+//    *
+//    */
+//   this.unhighlightPlace = function() {
+//     switch (_this.cat) {
+//       case "eat":
+//         _this.marker.setIcon(imgPath + 'eat.png');
+//         break;
+//       case "shop":
+//         _this.marker.setIcon(imgPath + 'shop.png');
+//         break;
+//       default:
+//         _this.marker.setIcon(imgPath + 'default.png');
+//     }
+//   }
+//
+//   // Assigns a click event listener to the marker to open the info window.
+//   this.addListener = google.maps.event.addListener(_this.marker,'click', (this.openInfowindow));
 };
 
 // Contains all the locations and search function.
 placesModel = {
-  locations:[],
-  query: ko.observable(''),
-};
-
-/**
- *
- */
-// Search function for filtering through the list of locations based on the name of the location.
-placesModel.search = ko.dependentObservable(function() {
-  const _this = this;
-  const search = this.query().toLowerCase();
-  return ko.utils.arrayFilter(_this.locations, function(location) {
-    const match =  location.name.toLowerCase().indexOf(search) >= 0;
-    if (!match) {
-      location.marker.setVisible(false);
-    } else {
-      location.marker.setVisible(true);
-    }
-    return match;
-  });
-}, placesModel);
-
-/**
- *
- */
-function initializePlaces() {
-  placesModel.locations = [
+  locations: [
     new Place('The Pavilion Shopping Center', 'shop', -29.849002300639423, 30.93577734073859),
     new Place('Westville Mall', 'shop', -29.83608, 30.918399),
     new Place('Kauai', 'eat', -29.83608, 30.918399),
@@ -232,11 +171,122 @@ function initializePlaces() {
     new Place('Waxy O\'Connors', 'eat', -29.827756663602152, 30.929725103495258),
     new Place('Lupa Osteria', 'eat', -29.8277474062012, 30.930414401226106),
     new Place('Chez nous', 'eat', -29.836469892379846, 30.91703684659349)
-  ];
+  ],
+  query: ko.observable(''),
+};
+
+/**
+ *
+ */
+// Search function for filtering through the list of locations based on the name of the location.
+placesModel.search = ko.computed(function() {
+  const _this = this;
+  const search = this.query().toLowerCase();
+  let searchResults = ko.utils.arrayFilter(_this.locations, function(location) {
+    const match =  location.name.toLowerCase().indexOf(search) >= 0;
+    if (!match) {
+      // location.marker.setVisible(false);
+    } else {
+      // location.marker.setVisible(true);
+    }
+    return match;
+  });
+
+  console.log('searchResults', searchResults);
+  if (_.isEmpty) {
+    searchResults = placesModel.locations;
+    console.log('searchResults default', searchResults);
+  }
+
+  return searchResults;
+}, placesModel);
+
+/**
+ *
+ */
+function initializePlaces() {
+  // placesModel.locations = [
+  //   new Place('The Pavilion Shopping Center', 'shop', -29.849002300639423, 30.93577734073859),
+  //   new Place('Westville Mall', 'shop', -29.83608, 30.918399),
+  //   new Place('Kauai', 'eat', -29.83608, 30.918399),
+  //   new Place('Olive & Oil Cafe', 'eat', 29.839529871456172, 30.925247375447384),
+  //   new Place('Waxy O\'Connors', 'eat', -29.827756663602152, 30.929725103495258),
+  //   new Place('Lupa Osteria', 'eat', -29.8277474062012, 30.930414401226106),
+  //   new Place('Chez nous', 'eat', -29.836469892379846, 30.91703684659349)
+  // ];
 }
+
+function setupMarkersForPlaces() {
+  _.forEach(placesModel.locations, (location) => {
+    location.marker = new google.maps.Marker({
+      position: new google.maps.LatLng(location.lng, location.lat),
+      animation: google.maps.Animation.DROP,
+      map,
+      name: location.name,
+      icon: location.icon
+    });
+
+    console.log('marker1 in loop', location.marker);
+    location.marker.infowindow = new google.maps.InfoWindow();
+
+    // Assigns a click event listener to the marker to open the info window.
+    location.marker.addListener = google.maps.event.addListener(location.marker, 'click', () => {
+      map.setCenter(location.marker.getPosition());
+      for (let i=0; i < placesModel.locations.length; i++) {
+        if (placesModel.locations[i].marker.infowindow) {
+          placesModel.locations[i].marker.infowindow.close();
+        }
+      }
+      map.panTo(location.marker.getPosition());
+      location.marker.infowindow.setContent(location.content);
+      location.marker.infowindow.open(map, location.marker);
+      _.defer(() => toggleMarkerBounceAnimation(location.marker));
+    });
+  });
+}
+
+/**
+ *
+ */
+GoogleMapsApiLoader({
+  libraries: ['places'],
+  apiKey: googleApiKey
+}).then(function (googleApi) {
+  initializeMap();
+  initializePlaces();
+  setupMarkersForPlaces();
+
+  $("span#arrow").click(() => {
+    console.log('click');
+    console.log($('span#arrow').html());
+    $("ul").slideToggle();
+
+    if ($('span#arrow').html() === '▼') {
+      $('span#arrow').html('▲');
+      $('div.search-area').css({'width': '100%'});
+    } else {
+      $('span#arrow').html('▼');
+      $('div.search-area').css({'width': 'auto'});
+    }
+  });
+  $( window ).resize(function() {
+    console.log('window resized');
+    if (isMobile()) {
+      $("ul").slideUp();
+    } else {
+      $("ul").slideDown();
+    }
+  });
+
+  console.log('okay slidedown');
+  $("ul").slideDown();
+}, function (err) {
+  console.error(err);
+});
 
 ko.applyBindings(placesModel);
 
+// TODO: Decouple 3rd party APIs and model
 // TODO: Responsive design, mobile-first if you can
 // NICE TO HAVE: MINIFY JS, CSS etc
 // TODO: When clicking on marker highlight item in listview
